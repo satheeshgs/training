c8b437937de3d60a382cc5d146a4da70f9f375f1
Satheesh Sundararajan <SESA445854@se.com>
2020-09-06 22:50:42 +0530

    Colors for mindful marble

---
+++ colors-mindful.py
@@ -0,0 +1,17 @@
+sky blue 1 -  #384b54 / rgb(56, 75, 84)
+sky blue 2 -  #507385 / rgb(80, 115, 133)
+sky blue 3 -  #6b9bb3 / rgb(107, 155, 179)
+sky blue 4 -  #adcad9 / rgb(173, 202, 217)
+sky blue 5 -  #445761 / rgb(68, 87, 97)
+sky blue 6 -  #839ca8 / rgb(131, 156, 168)
+sky blue 7 -  #a3b6bf / rgb(163, 182, 191)
+sky blue 8 -  #cfdee5 / rgb(207, 222, 229)
+sky blue 9 -  #edf1f2 / rgb(237, 241, 242)
+sky blue 10 - #ebf5fa / rgb(235, 245, 250)
+sky blue 11 - #f0f6f9 / rgb(240, 246, 249)
+sky blue 12 - #266484 / rgb(38, 100, 132)
+sky blue 13 - #00bcd4 / rgb(0, 188, 212)
+sky blue 14 - #00a5d4 / rgb(0, 165, 212)
+
+Orange 1 - #faca7d
+Orange 2 - #

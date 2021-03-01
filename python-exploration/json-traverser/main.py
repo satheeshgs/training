50bad6bcdb9b1e8fda76ec836a611cfd6b871006
Satheesh <satheesh.gowtham@gmail.com>
2021-03-01 13:14:33 +0530

    minor changes to main.py file

--- python-exploration/json-traverser/main.py
+++ python-exploration/json-traverser/main.py
@@ -7,15 +7,14 @@ def main():
     #ask for domain input from user and pass it to recursiveJson function
     json_data = pd.read_json("choose-domain.json")
     domains_available = []
+
+    #printing out the available domains list
     for i in range(0,len(json_data)):
         domains_available.append(json_data["name"][i])
     user_input = input(f"Please choose your domain: \n{domains_available}\n")
-    """
-    chosen_json = pd.read_json(returnDomainJson(domain))
-    """
+
     #getting the json filepath for traversing based on domain
-    #json_data = pd.read_json("choose-domain.json")
-    domain_json_path = returnDomainJson(json_data, user_input) 
+    domain_json_path = returnDomainJson(json_data, user_input)
     
     #setting json data to traverse based on domain
     json_to_traverse = pd.read_json(domain_json_path)
@@ -38,3 +37,4 @@ def main():
 #starting the main function
 if __name__ == "__main__":
     main()
+

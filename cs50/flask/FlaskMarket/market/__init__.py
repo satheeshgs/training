72bf749fa4a9a89f6c30a660bbe913b513a3eb1d
satheesh <satheesh.gowtham@gmail.com>
2021-08-31 01:01:58 +0530

    updated till flask forms

--- cs50/flask/FlaskMarket/market/__init__.py
+++ cs50/flask/FlaskMarket/market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'ceb6d0b35fc46ee52c560a5a'
 db = SQLAlchemy(app)
 
 from market import routes

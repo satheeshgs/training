6e8edf21f1d895f528de240b6443570eaf3b8867
satheesh <satheesh.gowtham@gmail.com>
2021-08-30 18:00:24 +0530

    home page first version

--- cs50/flask/FlaskMarket/market.py
+++ cs50/flask/FlaskMarket/market.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>
+@app.route("/home")
+def home_page():
+    return render_template("home.html")
+
+
+#@app.route("/about/<username>")
+#def about_page(username):
+#    return f"<h1>This is the about page of {username}</h1>"

d50ea1d2eb3e8e6eb8dd29dace894350d85a2e0a
satheesh <satheesh.gowtham@gmail.com>
2021-09-10 20:02:30 +0530

    uptill logout

--- cs50/flask/FlaskMarket/market/__init__.py
+++ cs50/flask/FlaskMarket/market/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'ceb6d0b35fc46ee52c560a5a'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 from market import routes
--- cs50/flask/FlaskMarket/market/routes.py
+++ cs50/flask/FlaskMarket/market/routes.py
@@ -1,8 +1,9 @@
 from market import app
 from flask import render_template, redirect, url_for, flash, get_flashed_messages
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
 from market import db
+from flask_login import login_user, logout_user
 
 @app.route("/")
 @app.route("/home")
@@ -30,3 +31,25 @@ def register_page():
             flash(f'There was an error createing a user: {err_msg}', category='danger')
     return render_template('register.html',form=form)
     
+
+@app.route("/login", methods=["GET", "POST"])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username= form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+            attempted_password=form.password.data
+            ):  
+            login_user(attempted_user)
+            flash(f"Success! You are logged in as {attempted_user.username}", category='succcess')
+            return redirect(url_for('market_page'))
+        else:
+            flash('Username and password do not match! Please try again', category='danger')
+    return render_template('login.html', form=form)
+
+
+@app.route("logout")
+def logout_page():
+    logout_user()
+    flash("You have been logged out!", category="info")
+    return redirect(url_for('home_page'))
